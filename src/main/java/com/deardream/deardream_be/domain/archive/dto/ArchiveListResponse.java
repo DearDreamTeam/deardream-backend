cffee25558b6b422e880177e87c7a98e5a786a03
hyesuhan <heasoo88@gmail.com>
2025-07-15 16:50:58 +0900

    fix: ArchiveListResponse 직렬화 오류 해결

--- src/main/java/com/deardream/deardream_be/domain/archive/dto/ArchiveListResponse.java
+++ src/main/java/com/deardream/deardream_be/domain/archive/dto/ArchiveListResponse.java
@@ -1,10 +1,13 @@
 package com.deardream.deardream_be.domain.archive.dto;
 
-import lombok.Builder;
+import lombok.*;
 
 import java.util.List;
 
 @Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class ArchiveListResponse {
     private int count;
     private List<ArchiveResponseDto> dtos;

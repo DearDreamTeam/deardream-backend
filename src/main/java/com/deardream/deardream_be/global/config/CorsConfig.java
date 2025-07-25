d3b9700fc8cbf668686c78474b023154609d9fa9
wlqgkrry <wlqgkrry@gmail.com>
2025-07-25 23:46:01 +0900

    [REFACTOR] cors 오류 해결

--- src/main/java/com/deardream/deardream_be/global/config/CorsConfig.java
+++ src/main/java/com/deardream/deardream_be/global/config/CorsConfig.java
@@ -22,7 +22,8 @@ public class CorsConfig implements WebMvcConfigurer {
                 "http://localhost:8080",
                 "http://localhost:3000",
                 "https://vote.dream.team",
-                "https://vote-dream.p-e.kr"));
+                "https://vote-dream.p-e.kr",
+                "https://www.deardream.site"));
 
         configuration.addAllowedHeader("*");
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));

305837ad0ae40ae5e33c3ae5d9c2096e011acfef
Yeongseo Kim <1903020@ewhain.net>
2025-07-28 16:58:00 +0900

    fix: default size 설정

--- src/main/java/com/deardream/deardream_be/domain/archive/service/PdfRender.java
+++ src/main/java/com/deardream/deardream_be/domain/archive/service/PdfRender.java
@@ -72,6 +72,7 @@ public class PdfRender {
             Files.copy(fontStream, tempFontFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
         }
         builder.useFont(tempFontFile, "Pretendard");
+        builder.useDefaultPageSize(794, 1123, PdfRendererBuilder.PageSizeUnits.POINTS);
         builder.toStream(baos);
         builder.withHtmlContent(resultHtml, "/");
         builder.run();
